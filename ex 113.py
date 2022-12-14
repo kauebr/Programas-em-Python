647f2d69c4d4dc6dff4274ca3488a54f6b1908e2
Kaue Mandarino <kauemandarino@gmail.com>
2022-12-14 07:54:51 -0300

    exercicios novos

--- ex 113.py
+++ ex 113.py
@@ -1,7 +1,4 @@
-# Exercício Python 104: Crie um programa que tenha a função leiaInt(), que vai funcionar de forma semelhante
-# ‘a função input() do Python, só que fazendo a validação para aceitar apenas um valor numérico.
-# Ex: n= leiaInt(‘Digite um n: ‘)
-def leiaInt(numero):
+'''def leiaInt(numero):
     valor = x
     while True:
         if x.isalpha() or valor.isalpha():
@@ -15,4 +12,6 @@ def leiaInt(numero):
 
 x = input('Digite um valor: ')
 leiaInt(x)
-print('E mais uma vez repito, você digitou:', x)
+print('E mais uma vez repito, você digitou:', x)'''
+
+x = input(int('Digite um número inteiro'))
