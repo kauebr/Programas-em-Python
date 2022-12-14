241fedffd8ee5b0ec81ea86da0336caf1b5bee7a
Kaue Mandarino <kauemandarino@gmail.com>
2022-12-14 08:17:36 -0300

    exercícios

--- ex 113.py
+++ ex 113.py
@@ -18,8 +18,15 @@ while flag != 1:
     try:
         x = int(input('Digite um número inteiro: '))
         flag = 1
-    except as erro:
-        print(f'Ops, tivemos um erro: {erro}')
+    except Exception as erro:
+        print(f'Ops, tivemos um erro: {erro.__class__}')
+flag = 0
+while flag != 1:
+    try:
+        x = float(input('Digite um número flutuante: '))
+        flag = 1
+    except Exception as erro:
+        print(f'Ops, tivemos um erro: {erro.__class__}')
 
 
 
